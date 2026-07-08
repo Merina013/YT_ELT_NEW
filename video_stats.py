1a3339353226ae4b05cb4cdd3fd01611f4814b2d
Merina Roy <merina.roy@Excelra.com>
2026-07-08 17:08:11 +0530

    Remove debug print statements

--- video_stats.py
+++ video_stats.py
@@ -1,32 +1,74 @@
 import requests
 import json
-API_KEY = "AIzaSyAR_ZaYz53rRjzpFor25qwVBfy9WAuLYyg"
+
+import os
+from dotenv import load_dotenv
+
+load_dotenv(dotenv_path="./.env")
+API_KEY = os.getenv("API_KEY")
 channel_handle = "MrBeast"
+maxResults = 50
+
 
 def get_playlist_id():
-
     try:
-
         url = f"https://youtube.googleapis.com/youtube/v3/channels?part=contentDetails&forHandle={channel_handle}&key={API_KEY}"
         response = requests.get(url)
         response.raise_for_status()
         data = response.json()
-        #print(json.dumps(data, indent=4))
+        # print(json.dumps(data, indent=4))
         channel_items = data['items'][0]
         channel_playlistId = channel_items['contentDetails']['relatedPlaylists']['uploads']
-        #print(channel_playlistId)
+        print(channel_playlistId)
         return channel_playlistId
 
     except requests.exceptions.RequestException as e:
         raise e
-    
+
+
+def get_video_ids(playlistId):
+
+    video_ids = []
+    pageToken = None
+
+    base_url = (
+        "https://youtube.googleapis.com/youtube/v3/playlistItems"
+        f"?part=contentDetails"
+        f"&playlistId={playlistId}"
+        f"&maxResults={maxResults}"
+        f"&key={API_KEY}"
+    )
+
+    try:
+        while True:
+
+            url = base_url
+
+            if pageToken:
+                url += f"&pageToken={pageToken}"
+
+            response = requests.get(url)
+            response.raise_for_status()
+            data = response.json()
+
+            print(json.dumps(data, indent=4))
+
+            for item in data.get('items', []):
+                video_id = item['contentDetails']['videoId']
+                video_ids.append(video_id)
+
+            pageToken = data.get('nextPageToken')
+
+            if not pageToken:
+                break
+
+        return video_ids
+
+    except requests.exceptions.RequestException as e:
+        raise e
+
+
 if __name__ == "__main__":
+    playlistId = get_playlist_id()
     print("getplaylist_id will be executed")
-    get_playlist_id()
-else:
-    print("getplaylist_id will not be executed")
-
-        
-
-
-
+    print(get_video_ids(playlistId))
